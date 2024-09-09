1c9551cd5029d113766dd5544fa691584185f287
bodsul <bodsul@gmail.com>
2024-09-09 19:22:01 +0000

    impl augmentations

--- src/dataset/mapillary_dataset.py
+++ src/dataset/mapillary_dataset.py
@@ -1,17 +1,49 @@
 import os
+import random
 from PIL import Image
 import torch
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+import albumentations as A
 
 N_LABELS = 124
 
+def make_augmentations(image_height, image_width, random_crop_min=0.6, p=0.5):
+    random_crop_ratio = random_crop_min + random.random()*(1-random_crop_min)
+    crop_height = int(random_crop_ratio*image_height)
+    crop_width = int(random_crop_ratio*image_width)
+
+    weak_transform = A.Compose([
+        A.RandomBrightnessContrast(p=p),
+        A.CLAHE(p=p),
+        A.Blur(blur_limit=3, p=p),
+        A.HueSaturationValue(p=p),
+    ])
+
+    strong_transform = A.Compose([
+        A.RandomCrop(width=crop_width, height=crop_height),
+        A.Resize(width=image_width, height=image_height),
+        A.HorizontalFlip(p=p),
+        A.OpticalDistortion(p=p),
+        A.ShiftScaleRotate(p=p),
+        A.RandomRotate90(p=p),
+        A.Transpose(p=p),
+        A.Perspective(p=p),
+    ])
+
+    return weak_transform, strong_transform
+
 class Mapillary_SemSeg_Dataset(Dataset):
-    def __init__(self, split, data_dir, version='v2.0'):
+    def __init__(self, split, data_dir, image_shape, augment=False, version='v2.0', verbose=False):
         self.data_dir = data_dir
         self.version = version
         self.split = split
+        self.image_height, self.image_width = image_shape
         self.image_filenames = os.listdir(os.path.join(self.data_dir, f'{split}/images'))
+        self.augment = augment
+        self.verbose = verbose
+        if self.augment:
+            self.weak_transform, self.strong_transform = make_augmentations(self.image_height, self.image_width)
 
     def __len__(self):
         return len(self.image_filenames)
@@ -22,20 +54,38 @@ class Mapillary_SemSeg_Dataset(Dataset):
         image_path = os.path.join(self.data_dir, f"{self.split}/images/{image_id}.jpg")
         label_path = os.path.join(self.data_dir, f"{self.split}/{self.version}/labels/{image_id}.png")
         
-        image = np.array(Image.open(image_path))
-        label = np.array(Image.open(label_path))
+        image = np.array(Image.open(image_path).resize((self.image_width, self.image_height)))
+        label = np.array(Image.open(label_path).resize((self.image_width, self.image_height), Image.NEAREST))
 
-        channel_labels = np.zeros((N_LABELS, *label.shape))
+        mask = np.zeros((*label.shape, N_LABELS))
 
         for j in range(N_LABELS):
-            channel_labels[j][label == j] = 1
+            mask[:, :, j][label == j] = 1
 
-        return image.transpose(2, 0, 1), channel_labels
+        if self.augment:
+            augment_mode = random.randint(0, 3)
+            if self.verbose:
+                print(f'augment_mode: {augment_mode}')
+            if augment_mode==0:
+                transformed = {'image': image, 'mask': mask}
+            if augment_mode==1:
+                transformed = self.weak_transform(image=image, mask=mask)
+            elif augment_mode==2:
+                transformed = self.strong_transform(image=image, mask=mask)
+            elif augment_mode==3:
+                transformed = self.weak_transform(image=image, mask=mask)
+
+                transformed = self.strong_transform(image=transformed['image'], mask=transformed['mask'])
+            
+            image = transformed['image']
+            mask = transformed['mask']
+        
+        return image.transpose(2, 0, 1), mask.transpose(2, 0, 1)
 
 
 if __name__ == '__main__':
-    train_dataset = Mapillary_SemSeg_Dataset('training', '../../data')
-    val_dataset = Mapillary_SemSeg_Dataset('validation', '../../data')
+    train_dataset = Mapillary_SemSeg_Dataset('training', '../../data', (512, 512), augment=True)
+    val_dataset = Mapillary_SemSeg_Dataset('validation', '../../data', (512, 512))
     # test_dataset = Mapillary_SemSeg_Dataset('testing', '../../data')
 
     img, label = train_dataset[0]
